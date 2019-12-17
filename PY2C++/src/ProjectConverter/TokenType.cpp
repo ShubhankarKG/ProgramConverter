e0f94883bcee87f9eecdd8bfb620ae15b05fdf54
Skg <shubhankar.gupto.11@gmail.com>
2019-12-17 20:48:40 +0530

    Directory structure changed for easier access. \n Trying to rewrite java files to cpp.

---
+++ PY2C++/src/ProjectConverter/TokenType.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+enum TokenType{
+    // Single character tokens
+    LEFT_PARAN, RIGHT_PARAN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+    // One or Two character tokens
+    NOT, NOT_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    //Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOFile
+
+};
